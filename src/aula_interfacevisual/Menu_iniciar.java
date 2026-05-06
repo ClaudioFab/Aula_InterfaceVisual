a9861907fbcf0b64af34b5c55b5341fd814433b0
ClaudioFab <cla.rob.fabiano@gmail.com>
2026-05-06 18:39:52 -0300

    adicionando nimbus

--- src/aula_interfacevisual/Menu_iniciar.java
+++ src/aula_interfacevisual/Menu_iniciar.java
@@ -149,6 +149,16 @@ public class Menu_iniciar extends javax.swing.JFrame {
 
     public static void main(String args[]) {
 
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         java.awt.EventQueue.invokeLater(() -> new Menu_iniciar().setVisible(true));
     }
